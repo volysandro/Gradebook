7a0f6f11f3e78ca1587239d0cbd3f8a4dfe49985
volysandro <volysandro@users.noreply.github.com>
2019-02-27 11:11:41 +0100

    v1

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(188, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Note hinzufügen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(702, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(183, 489);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Durchschnitt Berechnen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(3, 240);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(341, 65);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Note aus Punktzahl";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox5);
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox4);
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 489);
+            this.splitContainer1.SplitterDistance = 333;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(179, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.Location = new System.Drawing.Point(3, 311);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(327, 175);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(327, 231);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(3, 121);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(341, 113);
+            this.richTextBox5.TabIndex = 4;
+            this.richTextBox5.Text = "Max Score";
+            this.richTextBox5.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(174, 435);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(170, 51);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(3, 435);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 51);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Zum Notenbuch Hinzufügen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox4.Location = new System.Drawing.Point(3, 311);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(341, 118);
+            this.richTextBox4.TabIndex = 1;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.richTextBox3.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(341, 112);
+            this.richTextBox3.TabIndex = 0;
+            this.richTextBox3.Text = "Score";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 513);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        
+        public static double gradeToGradebook = 0;
+        
+        double[] grades = new double[100];
+        int gradeCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            double test = 0;
+
+            if (double.TryParse(textBox1.Text, out test))
+            {
+
+            if(textBox1.Text != "") {
+                double newGrade = Convert.ToDouble(textBox1.Text);
+                if (newGrade > 0 && newGrade < 6.1)
+                {
+                    richTextBox1.Text = "Your current grades are:";
+                    grades[gradeCounter] = newGrade;
+                    gradeCounter += 1;
+                    for (int x = 0; x < 100; x++)
+                    {
+                        if (grades[x] == 0) { break; }
+                        else
+                        {
+                            richTextBox1.AppendText(Environment.NewLine + grades[x]);
+                        }
+                    }
+                }
+
+                else
+                {
+                    textBox1.Text = "Enter Valid Grade Here";
+                }
+
+
+            }
+
+                else
+                {
+                    textBox1.Text = "Enter Valid Grade Here";
+                }
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+            else
+            {
+                textBox1.Text = "Enter Grade Here";
+            }
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double adder = 0;
+            for (int x = 0; x < 100; x++)
+            {
+                if (grades[x] == 0) { break; }
+                else
+                {
+                    adder = adder + grades[x];
+                }
+            }
+            double average = adder / gradeCounter;
+
+            if (average >= 4)
+            {
+                richTextBox2.ForeColor = Color.Green;
+            }
+            else { richTextBox2.ForeColor = Color.Red;
+            };
+            richTextBox2.Text = "The Average Grade Is:" + Environment.NewLine + Environment.NewLine + average;
+
+
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+            double test = 0;
+
+            if (double.TryParse(richTextBox5.Text, out test) && double.TryParse(richTextBox3.Text, out test))
+            {
+                if (richTextBox5.Text == "" || richTextBox3.Text == "")
+                {
+                if (richTextBox5.Text == "") { richTextBox5.Text = "Enter Maximum Score Here"; }
+                else { richTextBox3.Text = "Enter Score Here"; }
+
+                if (richTextBox3.Text == "") { richTextBox3.Text = "Enter Score Here"; }
+                else { richTextBox5.Text = "Enter Maximum Score Here"; }
+                }
+                else
+                {
+                double score = Convert.ToDouble(richTextBox3.Text);
+                double max = Convert.ToDouble(richTextBox5.Text);
+
+
+                double grade = score / max * 5 + 1;
+
+                if(grade >= 4) { richTextBox4.ForeColor = Color.Green; }
+                else { richTextBox4.ForeColor = Color.Red;  }
+
+                richTextBox4.Text = "The grade is:" + Environment.NewLine + Environment.NewLine + grade + Environment.NewLine + Environment.NewLine + "Rounded: " + Math.Round(grade, 2);
+                gradeToGradebook = Math.Round(grade, 2);
+                }
+
+                button4.Enabled = true;
+                
+            }
+
+
+            else if (!double.TryParse(richTextBox5.Text, out test))
+            {
+
+                richTextBox5.Text = "Enter VALID Maximum Score Here";
+                if (!double.TryParse(richTextBox3.Text, out test))
+                {
+                    richTextBox3.Text = "Enter VALID  Score Here";
+
+                }
+                else { }
+
+            }
+            else if (!double.TryParse(richTextBox3.Text, out test))
+            {
+                richTextBox3.Text = "Enter VALID  Score Here";
+
+            }
+
+
+
+
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+
+
+            if (gradeToGradebook > 0 && gradeToGradebook < 6.1)
+            {
+                richTextBox1.Text = "Your current grades are:";
+                grades[gradeCounter] = gradeToGradebook;
+                gradeCounter += 1;
+                for (int x = 0; x < 100; x++)
+                {
+                    if (grades[x] == 0) { break; }
+                    else
+                    {
+                        richTextBox1.AppendText(Environment.NewLine + grades[x]);
+                    }
+                }
+            }
+
+            else
+            {
+                richTextBox4.Text = "Calculate a Grade First";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox4.Text = "";
+            richTextBox3.Text = "";
+            richTextBox5.Text = "";
+            button4.Enabled = false;
+        }
+    }
+}
